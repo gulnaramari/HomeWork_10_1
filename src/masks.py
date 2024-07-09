ed7ee665db565b2aa1cbc690ccdc0c043b05c59a
gulnaramari <gulnaramari@yandex.ru>
2024-07-09 17:01:56 +0300

    редактирование окончательного кода

--- src/masks.py
+++ src/masks.py
@@ -27,4 +27,4 @@ def get_mask_account(input_data: str) -> str:
                 new_accountnumber += symbol
     slice_4 = new_accountnumber[-4:]
     mask_account = "**" + slice_4
-    return (f"{new_accountname} {mask_account}")
+    return (f"{new_accountname}  {mask_account}")
