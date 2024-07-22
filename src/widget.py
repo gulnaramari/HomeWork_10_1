3e7e40afab181c3ea188b92106453a0e203998c1
gulnaramari <gulnaramari@yandex.ru>
2024-07-22 18:23:00 +0300

    develop

--- src/widget.py
+++ src/widget.py
@@ -21,3 +21,4 @@ print(mask_account_card("Visa Platinum 7000792289606361"))
 print(mask_account_card("Maestro 7000792289606361"))
 print(mask_account_card("Счет 73654108430135874305"))
 print(get_data("2024-03-11T02:26:18.671407"))
+
