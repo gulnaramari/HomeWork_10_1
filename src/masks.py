5cbc040f61f6079ab069ff487481ce11cdfc4879
gulnaramari <gulnaramari@yandex.ru>
2024-07-26 13:18:21 +0300

    Финальное тестирование функции маскировки номера карты

--- src/masks.py
+++ src/masks.py
@@ -1,26 +1,30 @@
-import os
-
-
 def get_mask_card_number(input_data: str) -> str:
     """Функция, которая принимает на вход номер карты и возвращает ее маску"""
+    for arg in input_data:
+        if not isinstance(arg, str):
+            raise TypeError("Ошибка типа данных")
+
     new_cardnumber = ""
     new_cardname = ""
-    if "Visa Platinum" or "MasterCard" or "Maestro" in input_data:
+    if "Счет" in input_data:
+        raise ValueError("Некорректное название карты")
+    else:
         for symbol in input_data:
             if symbol.isdigit():
                 new_cardnumber += symbol
             elif symbol.isalpha():
                 new_cardname += symbol
 
-        if len(new_cardnumber) == 16:
-            """Случай нестандартной длины номера карты"""
+    if len(new_cardnumber) == 16:
+        if new_cardnumber[0] != "0":
             slice_1 = new_cardnumber[0:4]
             slice_2 = new_cardnumber[4:6]
             slice_3 = new_cardnumber[-4:]
             mask_card = slice_1 + " " + slice_2 + "** **** " + slice_3
         else:
-            raise ValueError("Неверный формат данных")
-
+            raise ValueError("Некорректный номер карты")
+    else:
+        raise ValueError("Некорректный номер карты")
     return (f"{new_cardname} {mask_card}")
 
 
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,4 +1,4 @@
-import os
+from typing import Any
 
 import pytest
 
@@ -7,59 +7,73 @@ from src.masks import get_mask_card_number
 
 @pytest.fixture
 def input_data() -> list[str]:
-    return [
-        "Visa Platinum 7000792289606361",
-        "Maestro 7000892289606361",
-        "Maestro 6769070082797565",
-        "MasterCard 6700000082797565"
-    ]
+    return ["Maestro 1596837868705199",
+            "Счет 64686473678894779589",
+            "MasterCard 7158300734726758",
+            "Счет 35383033474447895560",
+            "Visa Classic 6831982476737658",
+            "Visa Platinum 8990922113665229",
+            "Visa Gold 5999414228426353",
+            "Счет 73654108430135874305",
+            ]
 
 
 @pytest.mark.parametrize("input_data, expected_mask", [
-                        ("Visa Platinum 7000792289606361", "VisaPlatinum 7000 79** **** 6361"),
-                        ("Maestro 7000892289606361", "Maestro 7000 89** **** 6361"),
-                        ("Maestro 6769070082797565", "Maestro 6769 07** **** 7565"),
-                        ("MasterCard 6700000082797565", "MasterCard 6700 00** **** 7565"),
-])
-def test_get_mask_card_number(input_data: str, expected_mask: str) -> str | None:
+                        ("Visa Classic 6831982476737658", "VisaClassic 6831 98** **** 7658"),
+                        ("Visa Gold 5999414228426353", "VisaGold 5999 41** **** 6353"),
+                        ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+                        ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
+                         ])
+def test_get_mask_card_number(input_data: str, expected_mask: str) -> Any:
     assert get_mask_card_number(input_data) == expected_mask
 
 
-def test_get_invalid_mask_card_number():
+def test_get_invalid_mask_card_number(input_data: str) -> Any:
     with pytest.raises(ValueError):
         get_mask_card_number("12345678")
 
 
-def test_get_invalid_name_card_number():
+def test_get_invalid_name_card_number(input_data: str) -> Any:
     with pytest.raises(ValueError):
         get_mask_card_number("12opyu3456kljh78")
 
 
-def test_short_get_mask_card_number():
+def test_short_get_mask_card_number(input_data: str) -> Any:
     with pytest.raises(ValueError):
         get_mask_card_number("1")
 
 
-def test_get_empty_mask_card_number():
+def test_get_empty_mask_card_number(input_data: str) -> Any:
     with pytest.raises(ValueError):
-        get_mask_card_number("Hello")
+        get_mask_card_number("Visa Classic")
 
 
-def test_get_none_mask_card_number():
+def test_get_none_mask_card_number(input_data: str) -> Any:
     with pytest.raises(ValueError):
         get_mask_card_number("")
 
 
-def test_get_null_mask_card_number():
+def test_get_null_mask_card_number(input_data: str) -> Any:
     with pytest.raises(ValueError):
-        get_mask_card_number("0")
+        get_mask_card_number("0000123456789000")
 
 
-def test_get_format_card_number():
-    with pytest.raises(ValueError):
-        get_mask_card_number("*")
-
-
-def test_get_upper_format_name_card_number():
+def test_get_upper_format_name_card_number(input_data: str) -> Any:
     with pytest.raises(ValueError):
         get_mask_card_number("VISA")
+
+
+def test_get_uncorrect_name_card_number(input_data: str) -> Any:
+    with pytest.raises(ValueError):
+        get_mask_card_number("Счет 73654108430135874305")
+
+
+@pytest.mark.parametrize("input_data", [
+                        (6831982476737658),
+                        (2345),
+                        (1098765),
+                        (7158300734726758),
+                         ])
+def test_get_mask_card_number_wrong_type(input_data: str) -> Any:
+    with pytest.raises(TypeError):
+        get_mask_card_number(input_data)
